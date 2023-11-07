45cd00ab66eab96235b3a31b607b0e7a112ac801
Tuan031224 <149644702+Tuan031224@users.noreply.github.com>
2023-11-07 15:46:10 +0700

    Update header.php

--- view/header.php
+++ view/header.php
@@ -25,7 +25,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang Chủ 123</title>
+    <title>Trang Chủ</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
